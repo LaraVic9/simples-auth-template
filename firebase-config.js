3edd1105c51a34e1065a7a2b426e0e024001235c
LaraVic9 <laravicspereira@gmail.com>
2023-09-14 17:35:24 -0300

    Adicionando pasta atual ao repositório

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCfdIxO_TaYtTqo5pgzG4uuauto8XRdGG0",
+  authDomain: "teste-project-fa3c2.firebaseapp.com",
+  projectId: "teste-project-fa3c2",
+  storageBucket: "teste-project-fa3c2.appspot.com",
+  messagingSenderId: "184756516083",
+  appId: "1:184756516083:web:ac5a04f0bc4136cf5cf386",
+  measurementId: "G-VLSBGZE2Q3"
+};
+
